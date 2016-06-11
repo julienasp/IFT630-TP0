5855094384b231336a014c4cca3703377d2b8e81
julienasp <julien.aspirot@usherbrooke.ca>
2016-06-11 18:38:26 -0400

    all right for us

--- services/T3PipelineService.java
+++ services/T3PipelineService.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package services;
+import core.Job;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import utils.*;
 
 /**
@@ -15,7 +18,47 @@ public class T3PipelineService extends ServicePipeline implements Runnable {
     @Override
     public void run() {
         Log.log("T3 Service Thread running...");
-        
+    boolean run = true;
+        try {
+            while(run){
+            
+                Log.log("T3 Service Thread: waiting for a new job...");
+                Job currentJob = this.popJob();
+                
+                if(currentJob != null){
+                    Log.log("T3 Service Thread: the file: " + currentJob.getJobName() + " is being process...");                    
+                    
+                    //add color to java keywords                    
+                    Pattern pattern = Pattern.compile("(\\/\\*(\\*(?!\\/)|[^*])*\\*\\/|\\/\\/.*)");
+                    Matcher matcher = pattern.matcher(currentJob.getContent());
+                    StringBuffer sb = new StringBuffer(currentJob.getContent().length());
+                    while (matcher.find()) {                        
+                        matcher.appendReplacement(sb, Matcher.quoteReplacement("<span style=\"color:green\">"+matcher.group(1)+"</span>"));                        
+                    }                   
+                    matcher.appendTail(sb);
+                    currentJob.setContent(sb.toString());
+                    
+                    this.getNextService().addJob(currentJob);
+                     
+                    if(currentJob.isLastJob()){
+                        Log.log("T3 Service Thread: the 'NoMoreJob' flag has been detected.");
+                        run = false;
+                    }                    
+                }
+                else{
+                    Thread.currentThread().sleep(10); // IF EMPTY WE SLEEP FOR A WHILE... TO GIVE TIME TO THE PRODUCER
+                }
+                
+            }
+        } catch (Exception ex) {
+                Log.log("T3 Service Thread: Exception: " + ex.getMessage());
+        } finally {
+            stop();
+	}
+    }
+    private void stop(){
+        Log.log("T3 Service Thread: is being stop...."); 
+        Thread.currentThread().interrupt();
     }
     
     
