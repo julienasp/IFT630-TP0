eb2c119fd889421f20573f0094d6670e53872d0e
julienasp <julien.aspirot@usherbrooke.ca>
2016-06-12 12:57:50 -0400

    oupsi

--- services/T2PipelineService.java
+++ services/T2PipelineService.java
@@ -35,7 +35,8 @@ public class T2PipelineService extends ServicePipeline implements Runnable {
                     Pattern pattern = Pattern.compile("(alignas|alignof| and |and_eq|asm|atomic_cancel|atomic_commit|atomic_noexcept|auto |bitand|bitor|bool |break|case|catch|char |char16_t|char32_t|class|compl|vconcept|const|constexpr|const_cast|continue|decltype|default|delete|do |double|dynamic_cast|else|enum|explicit|export|extern|false|float|for|friend|goto|if|inline|int |import|long |module|mutable|namespace|new|noexcept|not|not_eq|nullptr|operator| or |or_eq|private|protected|public|register|reinterpret_cast|requires|return |short |signed|sizeof|static|static_assert|static_cast|struct|switch|synchronized|template|this|thread_local|throw|true|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while|xor|xor_eq|override|final|transaction_safe|transaction_safe_dynamic)");
                     Matcher matcher = pattern.matcher(currentJob.getContent());
                     StringBuffer sb = new StringBuffer(currentJob.getContent().length());
-                    while (matcher.find()) {                        
+                    while (matcher.find()) {
+                        matcher.appendReplacement(sb, Matcher.quoteReplacement("<span class=\"keyword\">"+matcher.group(1)+"</span>"));
                     }                   
                     matcher.appendTail(sb);
                     currentJob.setContent(sb.toString());
